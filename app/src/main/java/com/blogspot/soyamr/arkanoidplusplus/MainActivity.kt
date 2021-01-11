b4125647e453694a7f89a2142533bbc008d64153
•Daniil KriDan • <q-2-013@mail.ru>
2021-01-11 23:15:46 +0700

    main screen done

--- app/src/main/java/com/blogspot/soyamr/arkanoidplusplus/MainActivity.kt
+++ app/src/main/java/com/blogspot/soyamr/arkanoidplusplus/MainActivity.kt
@@ -2,15 +2,32 @@ package com.blogspot.soyamr.arkanoidplusplus
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : Activity() {
+
+    // animated background
+    var animationDrawable: AnimationDrawable? = null
+    var frameLayout:FrameLayout? = null
+    // animated background
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, LevelSelectActivity::class.java))
+        // declare animation and frameLayout
+        frameLayout=findViewById(R.id.mainFrameLay)
+        animationDrawable= frameLayout!!.background as AnimationDrawable?
+        // add time changes
+        animationDrawable!!.setEnterFadeDuration(5000)
+        animationDrawable!!.setExitFadeDuration(2000)
+        // start animation
+        animationDrawable!!.start()
+
+        //startActivity(Intent(this, LevelSelectActivity::class.java))
     }
 
 }
