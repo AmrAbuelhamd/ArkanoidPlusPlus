a1c2fb346a8f3692f3df992eb9def8aedc0377cb
•Daniil KriDan • <q-2-013@mail.ru>
2021-01-17 13:18:30 +0700

    congratulations screen

---
+++ app/src/main/java/com/blogspot/soyamr/arkanoidplusplus/CongratulationsActivity.kt
@@ -0,0 +1,11 @@
+package com.blogspot.soyamr.arkanoidplusplus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CongratulationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_congratulations)
+    }
+}
