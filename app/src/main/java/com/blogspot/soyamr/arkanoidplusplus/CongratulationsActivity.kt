82c660a714d65d07ee481b248e60cbe0957e1a76
•Daniil KriDan • <q-2-013@mail.ru>
2021-01-17 13:50:13 +0700

    icon added

--- app/src/main/java/com/blogspot/soyamr/arkanoidplusplus/CongratulationsActivity.kt
+++ app/src/main/java/com/blogspot/soyamr/arkanoidplusplus/CongratulationsActivity.kt
@@ -1,9 +1,10 @@
 package com.blogspot.soyamr.arkanoidplusplus
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CongratulationsActivity : AppCompatActivity() {
+class CongratulationsActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_congratulations)
