616ce0b6547b16321823bc8e4910b82ded33f468
•Daniil KriDan • <q-2-013@mail.ru>
2021-01-17 18:09:43 +0700

    level select args

--- app/src/main/java/com/blogspot/soyamr/arkanoidplusplus/level_select/LevelSelectFragment.kt
+++ app/src/main/java/com/blogspot/soyamr/arkanoidplusplus/level_select/LevelSelectFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import com.blogspot.soyamr.arkanoidplusplus.R
+import com.blogspot.soyamr.arkanoidplusplus.Repository
 import com.blogspot.soyamr.arkanoidplusplus.game_stuff.GameActivity
 import kotlinx.android.synthetic.main.fragment_level_select.*
 import kotlinx.android.synthetic.main.fragment_level_select.view.*
@@ -14,10 +17,16 @@ import kotlinx.android.synthetic.main.fragment_level_select.view.*
 
 class LevelSelectFragment : Fragment() {
 
+    // repository
+    private lateinit var repository: Repository
+
+    var iconID:Int?=-1
+    private lateinit var imageView: ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        repository = Repository(requireContext())
     }
 
     override fun onCreateView(
@@ -25,7 +34,13 @@ class LevelSelectFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val args = arguments?.let { LevelSelectFragmentArgs.fromBundle(it) }
+        iconID = args?.iconID
         val view =  inflater.inflate(R.layout.fragment_level_select, container, false)
+        if (iconID != -1) {
+            imageView = view.findViewById(R.id.imageViewLevelSelect)
+
+        }
 
         view.buttonLevel1.setOnClickListener{
             startActivity(Intent(requireContext(), GameActivity::class.java))
