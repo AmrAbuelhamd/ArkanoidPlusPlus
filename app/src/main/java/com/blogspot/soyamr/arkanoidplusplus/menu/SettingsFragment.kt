35aa6b7624410059f7b15bbe4d4c4a5c2720dfc4
•Daniil KriDan • <q-2-013@mail.ru>
2021-01-18 15:26:22 +0700

    still have problms with settings

--- app/src/main/java/com/blogspot/soyamr/arkanoidplusplus/menu/SettingsFragment.kt
+++ app/src/main/java/com/blogspot/soyamr/arkanoidplusplus/menu/SettingsFragment.kt
@@ -1,13 +1,13 @@
 package com.blogspot.soyamr.arkanoidplusplus.menu
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.RadioButton
-import android.widget.Switch
+import android.widget.*
+import android.widget.RadioGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.blogspot.soyamr.arkanoidplusplus.R
 
@@ -29,6 +29,8 @@ class SettingsFragment : Fragment() {
     var switchSound: Switch? = null
     var switchMusic: Switch? = null
 
+    var radioGroupPlatformControl: RadioGroup? = null
+
     var radioButtonTouch: RadioButton? = null
     var radioButtonGyroscope: RadioButton? = null
 
@@ -38,7 +40,7 @@ class SettingsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
-
+        val intent = Intent(requireContext(), MainActivity::class.java)
         saveButton = view.findViewById(R.id.buttonSave)
         goBackButton = view.findViewById(R.id.buttonGoBack3)
 
@@ -47,10 +49,68 @@ class SettingsFragment : Fragment() {
         radioButtonTouch = view.findViewById(R.id.radioButtonSettings1)
         radioButtonGyroscope = view.findViewById(R.id.radioButtonSettings2)
 
-        musicON = this.requireArguments().getBoolean("music", true)
-        soundON = this.requireArguments().getBoolean("sound", true)
-        touchON = this.requireArguments().getBoolean("touch", true)
-        
+        intent.putExtra("music", true)
+        musicON = requireArguments().getBoolean("music", true)
+        soundON = requireArguments().getBoolean("sound", true)
+        touchON = requireArguments().getBoolean("touch", true)
+
+        switchMusic!!.isChecked = musicON
+        switchSound!!.isChecked = soundON
+        if (touchON){
+            radioButtonTouch!!.isChecked = true
+        }
+        else
+        {
+            radioButtonGyroscope!!.isChecked = true
+        }
+
+        switchMusic!!.setOnCheckedChangeListener{ compoundButton: CompoundButton, b: Boolean ->
+            if (b)
+            {
+                intent.putExtra("music", true)
+            }
+            else
+            {
+                intent.putExtra("music", false)
+            }
+        }
+
+        switchSound!!.setOnCheckedChangeListener{ compoundButton: CompoundButton, b: Boolean ->
+            if (b)
+            {
+                intent.putExtra("sound", true)
+            }
+            else
+            {
+                intent.putExtra("sound", false)
+            }
+        }
+
+        radioGroupPlatformControl!!.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
+            // This will get the radiobutton that has changed in its check state
+            if (checkedId == radioButtonTouch!!.id)
+            {
+                intent.putExtra("touch", true)
+            }
+            else
+            {
+                intent.putExtra("touch", false)
+            }
+
+/*            val checkedRadioButton = group.findViewById<View>(checkedId) as RadioButton
+            // This puts the value (true/false) into the variable
+            val isChecked = checkedRadioButton.isChecked
+            // If the radiobutton that has changed in check state is now checked...
+            if (isChecked) {
+                // Changes the textview's text to "Checked: example radiobutton text"
+                tv.setText("Checked:" + checkedRadioButton.text)
+            }*/
+        })
+
+
+
+
+
         saveButton!!.setOnClickListener{
             //TODO
         }
